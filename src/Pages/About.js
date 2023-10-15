8033a66e70476a5ad0090b5012b9533e3a856d45
Mixaqi <mixaqimixaqi@mail.ru>
2023-10-15 16:13:03 +0300

    added new buttons and info in about

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,59 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { NavItem } from "react-bootstrap";
+import { Container, Tab, Nav, Row, Col } from "react-bootstrap";
+import drugs1 from "./drugs1.jpeg";
+import drugs2 from "./drugs2.webp";
+import drugs3 from "./drugs3.jpeg";
 
-export default class About extends Component{
-    render(){
-        return(
-            <div>
-                About
-            </div>
+export default class About extends Component {
+    render() {
+        return (
+            <Container>
+                <Tab.Container id="ledt-tabs-example" defaultActiveKey="first">
+                    <Row>
+                        <Col sm={3}>
+                            <Nav variant="pills" className="flex-column mt-2">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">TAB 1</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">TAB 2</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">TAB 3</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col sm={9}>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                <img src={drugs1} alt="Drugs 1" />
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                                        unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                <img src={drugs2} alt="Drugs 2" />
+
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                                        unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                <img src={drugs3} alt="Drugs 3" />
+
+                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                                        unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
+            </Container>
         )
     }
 }
