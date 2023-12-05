4b8ae8796d0229d490d4e18123ea853b7d741911
Mixaqi <mixaqimixaqi@mail.ru>
2023-12-05 18:33:38 +0300

    added map centering after marker deletion

--- src/Pages/Report.jsx
+++ src/Pages/Report.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const Report = () => {
@@ -19,18 +19,45 @@ const Report = () => {
     setMarkers(updatedMarkers);
   };
 
+  const handleMarkerDrag = (index, newMarker) => {
+    const updatedMarkers = [...markers];
+    updatedMarkers[index] = newMarker;
+    setMarkers(updatedMarkers);
+  };
+
+  useEffect(() => {
+
+    if (markers.length > 0) {
+      const bounds = new window.google.maps.LatLngBounds();
+      markers.forEach((marker) => {
+        bounds.extend(marker);
+      });
+
+      const newCenter = {
+        lat: bounds.getCenter().lat(),
+        lng: bounds.getCenter().lng(),
+      };
+
+      setMapCenter(newCenter);
+    }
+  }, [markers]);
+
+  const [mapCenter, setMapCenter] = useState({ lat: 0, lng: 0 });
+
   return (
     <LoadScript googleMapsApiKey="AIzaSyBbew2lrwBi8ibxBzzO_0wO24e4p2cujF8">
       <GoogleMap
         mapContainerStyle={{ height: '400px', width: '100%' }}
         zoom={10}
-        center={{ lat: 0, lng: 0 }}
+        center={mapCenter}
         onClick={handleMapClick}
       >
         {markers.map((marker, index) => (
           <Marker
             key={index}
             position={marker}
+            draggable={true}
+            onDragEnd={(e) => handleMarkerDrag(index, { lat: e.latLng.lat(), lng: e.latLng.lng() })}
             onClick={() => deleteMarker(index)}
           />
         ))}
