902c337545b45f493dcaeb93b54168ed2c771c60
Mixaqi <mixaqimixaqi@gmail.com>
2023-12-14 12:58:32 +0300

    added json sending in Report

--- src/Pages/Report.jsx
+++ src/Pages/Report.jsx
@@ -1,36 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useState, useEffect } from 'react'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
 const Report = () => {
-  const [markers, setMarkers] = useState([]);
-  const [textData, setTextData] = useState('');
-  const [mapCenter, setMapCenter] = useState({ lat: 0, lng: 0 });
+  const [markers, setMarkers] = useState([])
+  const [textData, setTextData] = useState('')
+  const [mapCenter, setMapCenter] = useState({ lat: 0, lng: 0 })
 
   const handleMapClick = (event) => {
     const newMarker = {
       lat: event.latLng.lat(),
       lng: event.latLng.lng(),
-    };
+    }
 
-    setMarkers([...markers, newMarker]);
-  };
+    setMarkers([...markers, newMarker])
+  }
 
   const deleteMarker = (index) => {
-    const updatedMarkers = [...markers];
-    updatedMarkers.splice(index, 1);
-    setMarkers(updatedMarkers);
-  };
+    const updatedMarkers = [...markers]
+    updatedMarkers.splice(index, 1)
+    setMarkers(updatedMarkers)
+  }
 
   const handleMarkerDrag = (index, newMarker) => {
-    const updatedMarkers = [...markers];
-    updatedMarkers[index] = newMarker;
-    setMarkers(updatedMarkers);
-  };
+    const updatedMarkers = [...markers]
+    updatedMarkers[index] = newMarker
+    setMarkers(updatedMarkers)
+  }
 
   const handleSendData = () => {
-    console.log('Sending data:', { markers, textData });
-    // добавить отправку на сервер
-  };
+    const dataToSend = {
+      markers,
+      textData,
+    }
+
+    console.log('Data to send:', dataToSend)
+
+    fetch('your_api_endpoint', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataToSend),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Data sent successfully:', data)
+      })
+      .catch((error) => {
+        console.error('Error sending data:', error)
+      })
+  }
 
   return (
     <LoadScript googleMapsApiKey="AIzaSyBbew2lrwBi8ibxBzzO_0wO24e4p2cujF8">
@@ -45,7 +64,12 @@ const Report = () => {
             key={index}
             position={marker}
             draggable={true}
-            onDragEnd={(e) => handleMarkerDrag(index, { lat: e.latLng.lat(), lng: e.latLng.lng() })}
+            onDragEnd={(e) =>
+              handleMarkerDrag(index, {
+                lat: e.latLng.lat(),
+                lng: e.latLng.lng(),
+              })
+            }
             onClick={() => deleteMarker(index)}
           />
         ))}
@@ -60,7 +84,7 @@ const Report = () => {
       </div>
       <button onClick={handleSendData}>Отправить данные</button>
     </LoadScript>
-  );
-};
+  )
+}
 
-export default Report;
+export default Report
