f56b665799e9cd082fc9e7e75b16724516ed507e
Mixaqi <mixaqimixaqi@mail.ru>
2023-12-26 00:23:20 +0300

    fixed reports form

--- client/src/Pages/Report.jsx
+++ client/src/Pages/Report.jsx
@@ -1,40 +1,92 @@
-import React, { useState } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useState, useEffect } from 'react'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
 const Report = () => {
-  const [markers, setMarkers] = useState([]);
-  const [textData, setTextData] = useState('');
-  const [mapCenter] = useState({ lat: 0, lng: 0 });
+  const [markers, setMarkers] = useState([])
+  const [textData, setTextData] = useState('')
+  const [mapCenter] = useState({ lat: 0, lng: 0 })
+  const [email, setEmail] = useState('')
+  const [photo, setPhoto] = useState(null)
+  const [emailDirty, setEmailDirty] = useState(false)
+  const [emailError, setEmailError] = useState('Email не может быть пустым')
+  const [formValid, setFormValid] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  useEffect(() => {
+    if (emailError) setFormValid(false)
+    else setFormValid(true)
+  }, [emailError])
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value)
+    const re =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    if (!re.test(String(e.target.value).toLowerCase()))
+      setEmailError('Некорректный email')
+    else setEmailError('')
+  }
 
   const handleMapClick = (event) => {
     const newMarker = {
       lat: event.latLng.lat(),
       lng: event.latLng.lng(),
-    };
+    }
 
-    setMarkers([...markers, newMarker]);
-  };
+    setMarkers([...markers, newMarker])
+  }
 
   const deleteMarker = (index) => {
-    const updatedMarkers = [...markers];
-    updatedMarkers.splice(index, 1);
-    setMarkers(updatedMarkers);
-  };
+    const updatedMarkers = [...markers]
+    updatedMarkers.splice(index, 1)
+    setMarkers(updatedMarkers)
+  }
 
   const handleMarkerDrag = (index, newMarker) => {
-    const updatedMarkers = [...markers];
-    updatedMarkers[index] = newMarker;
-    setMarkers(updatedMarkers);
-  };
+    const updatedMarkers = [...markers]
+    updatedMarkers[index] = newMarker
+    setMarkers(updatedMarkers)
+  }
 
-  const handleSendData = async (e) => {
-    e.preventDefault();
+  const handlePhotoChange = (e) => {
+    const selectedPhoto = e.target.files[0]
+    setPhoto(selectedPhoto)
+  }
 
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'email':
+        setEmailDirty(true)
+        break
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (isSubmitting) {
+      return
+    }
+    try {
+      setIsSubmitting(true)
+      await handleSendData
+      handleSendData()
+      setEmail('')
+      setEmailDirty(false)
+      setEmailError('Email не может быть пустым')
+      setPhoto(null)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsSubmitting(true)
+    }
+  }
+
+  const handleSendData = async () => {
     const dataToSend = {
       markers,
       textData,
-    };
-
+      email,
+      photo,
+    }
     try {
       const response = await fetch('your_api_endpoint', {
         method: 'POST',
@@ -42,23 +94,22 @@ const Report = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(dataToSend),
-      });
+      })
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error('Network response was not ok')
       }
-      setMarkers([]);
-      setTextData('');
 
-      const data = await response.json();
-      console.log('Data sent successfully:', data);
+      const data = await response.json()
+      console.log('Successfully:', data)
     } catch (error) {
-      console.error('Error sending data:', error);
+      console.error('Error sending data:', error)
+      throw error
     }
-  };
+  }
 
   return (
-    <form onSubmit={handleSendData} style={{width: '100%'}}>
+    <form onSubmit={handleSendData} style={{ width: '100%' }}>
       <LoadScript googleMapsApiKey="AIzaSyBbew2lrwBi8ibxBzzO_0wO24e4p2cujF8">
         <GoogleMap
           mapContainerStyle={{ height: '60vh', width: '100%' }}
@@ -89,10 +140,26 @@ const Report = () => {
             style={{ height: '15vh', width: '100%', resize: 'vertical' }}
           />
         </div>
-        <button type="submit">Отправить данные</button>
+        {emailDirty && emailError && <div className="error">{emailError}</div>}
+        <input
+          onChange={(e) => emailHandler(e)}
+          value={email}
+          onBlur={(e) => blurHandler(e)}
+          name="email"
+          type="text"
+          placeholder="Введите email"
+        />
+        <input
+          type="file"
+          onChange={(e) => setPhoto(e.target.files[0])}
+          accept="image/*"
+        />
+        <button disabled={!formValid} type="submit">
+          Отправить{' '}
+        </button>
       </LoadScript>
     </form>
-  );
-};
+  )
+}
 
-export default Report;
+export default Report
