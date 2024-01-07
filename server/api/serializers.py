8bb8fc345034ca4412401e3cc6d9e86329064166
Mixaqi <mixaqimixaqi@mail.ru>
2024-01-07 14:49:51 +0300

    added serializers for auth

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -2,9 +2,12 @@ from __future__ import annotations
 
 from typing import Any
 
+from authentication.models import User
 from contacts.models import Appeal
+from django.contrib.auth.password_validation import validate_password
 from report.models import Report
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class AppealSerializer(serializers.ModelSerializer):
@@ -29,3 +32,49 @@ class ReportSerializer(serializers.ModelSerializer):
         report.is_complete = False
         report.save()
         return report
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email"]
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user) -> dict[str, Any]:
+        token = super().get_token(user)
+        token["full_name"] = user.profile.full_name
+        token["username"] = user.username
+        token["email"] = user.profile.email
+        token["bio"] = user.profile.bio
+        token["image"] = user.profile.image
+        token["verified"] = user.profile.verified
+        return token
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only = True, required = True, validators = [validate_password],
+    )
+    confirmed_password = serializers.CharField(
+        write_only = True, required = True,
+    )
+    class Meta:
+        model = User
+        fields = ["email", "username", "password", "confirmed_password"]
+
+        def validate(self, attrs: dict[str, Any]) -> dict[str, Any]:
+            if attrs["password"] != attrs["confirmed_password"]:
+                raise serializers.ValidationError(
+                    {"password": "Password fields does not match"},
+                )
+            return attrs
+
+        def create(self, validated_data: dict[str, Any]) -> User:
+            user = User.objects.create(
+                username=validated_data["username"],
+                email=validated_data["email"],
+            )
+            user.set_password(validated_data["password"])
+            user.save()
+
+            return user
+
