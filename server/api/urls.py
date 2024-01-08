68f1ce780bf9f7cc111a746e4f70424c60c6958b
Mixaqi <mixaqimixaqi@mail.ru>
2024-01-08 20:21:57 +0300

    added front routing and axios

--- server/api/urls.py
+++ server/api/urls.py
@@ -7,15 +7,18 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from api import views
 from api.views import AppealViewSet, ReportViewSet
 
+from . import views
+
 router = DefaultRouter()
 router.register("appeals", AppealViewSet)
 router.register("reports", ReportViewSet)
 
 urlpatterns = [
-    path("token/", views.MyTokenObtainPairView.as_view()),
-    path("token/refresh/", TokenRefreshView.as_view()),
-    path("register/", views.RegisterView.as_view()),
-    path("dashboard/", views.dashboard),
+    path("token/", views.MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", views.RegisterView.as_view(), name="auth_register"),
+    #path("dashboard/", views.dashboard),
+    #path("", views.getRoutes),
 ]
 
 urlpatterns.extend(router.urls)
